94bf5c07b702867a0cb9a3d489790114813e02c1
vasu9111 <vasugondaliya1510@gamil.com>
2024-10-15 12:18:45 +0530

    fix: add joi lib for custom validation
    Remove all custom validation and usejoi library for validation as middleware

--- book/bookRoutes.js
+++ book/bookRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bookController = require('./bookController');
+const { validate, idNotFound } = require('../middleware/bookMiddleware');
+
+const { bookSchema, updateBookSchema } = require('./bookValidation');
 const router = express.Router();
 
 router.get('/all-books', bookController.getAllBooks);
-router.post('/all-books', bookController.getFilteredBooks);
-router.get('/:id', bookController.getSingleBook);
-router.post('/add', bookController.addBook);
-router.put('/:id', bookController.updateBook);
-router.delete('/:id', bookController.deleteBook);
+router.post('/filter', bookController.getFilteredBooks);
+router.get('/:id', idNotFound, bookController.getSingleBook);
+router.post('/add', validate(bookSchema), bookController.addBook);
+router.put('/:id', idNotFound, validate(updateBookSchema), bookController.updateBook);
+router.delete('/:id', idNotFound, bookController.deleteBook);
 
 module.exports = router;
